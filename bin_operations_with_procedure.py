c41c3be6c0142d8b6962896647df7e41102a2a7c
3rickDJ <erick.dejesus1@alumno.buap.mx>
2022-02-01 08:21:40 -0600

    added substraction between 2 numbers

--- bin_operations_with_procedure.py
+++ bin_operations_with_procedure.py
@@ -1,4 +1,5 @@
 
+
 def multiplication():
     # base = int(input('In wich base are you working on?: '))
     # a = int(input('Type the first number please: '), base)
@@ -9,9 +10,11 @@ def multiplication():
     
     a_for_printing = ' '.join(list(a))
     bar = '\u2500'*len(a)+ '\u2500'*3
+    #print a bar and factors
     print(f'{a:>30}\n{"x "+b:>30}\n{bar:>30}')
 
     # operations=''
+    #print the procedure
     for i, digit in zip(range(0,len(b)),  b[::-1]):
         if digit=='1':
             print(f'{a_for_printing +" ":>{30-i*2}}')
@@ -20,6 +23,7 @@ def multiplication():
     product = bin(int(a,2)*int(b,2))[2:]
     # print(' '*29 + '\u2500'*len(product*2))
     bar = '\u2500'*4+'\u2500'*2*len(product)
+    #print bar and producta
     print(f'{bar:>30}')
     product = ' '.join(list(product))
     print(f'{product+" ":>30}')
@@ -32,7 +36,27 @@ def divition():
 
 
 def substraction():
-    pass
+    a = input('Type the minuend   : ')
+    b = input('Type the subtrahend: ')
+    bar = '\u2500'*len(a)+ '\u2500'*3
+    difference = int(a,2)-int(b,2)
+    if difference <0:
+        base = 4
+        difference = (1<<base) + difference
+    print(f'{a:>30}\n{"- "+b:>30}\n{bar:>30}\n{difference:>30b}')
+    
+    print('\n' + b + ' ==> ')
+    twos_complement(int(b,2),4)
+    print(f'{"+ "+a:>37}\n{bar:>37}\n{difference:>37b}')
+
+
+
+def twos_complement(a, bits):
+    a = ((1<<bits)-1) ^ a
+    print(f'ones = {a:>30b}\n')
+    a +=1
+    print(f'twos = {a:>30b}')
+    return a
 
 
 
@@ -52,6 +76,8 @@ def main():
         divition()
     elif option =='3':
         substraction()
+    elif option == '4':
+        twos_complement(-4,8)   
     else:
         print('Choose a correct option')
     # except:
